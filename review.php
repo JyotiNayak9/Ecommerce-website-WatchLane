477fe2e174c1d45775229048ddb3152b7fc32bfd
JyotiNayak9 <nayakjyoti789@gmail.com>
2023-09-13 10:34:21 +0545

    customer review

---
+++ review.php
@@ -0,0 +1,16 @@
+<?php
+$con=mysqli_connect('localhost','root');
+if($con){
+    echo"connection succesfull";
+}else{
+    echo"No connection";
+}
+mysqli_select_db($con,'watchdata');
+$username=$_POST['username'];
+$email=$_POST['email'];
+$comment=$_POST['comment'];
+$query="insert into review (username, email, comment) 
+values('$username','$email','$comment')";
+mysqli_query($con,$query);
+
+?>
