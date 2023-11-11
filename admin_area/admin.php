51219de1c8a9438260ebefbe642203955a025713
JyotiNayak9 <nayakjyoti789@gmail.com>
2023-11-11 23:24:36 +0545

    admin area

---
+++ admin_area/admin.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Admin dashboard</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' href='admin.css'>
+</head>
+<body>
+  <div class="navbar">
+    <div class="lgoo">
+    <h2 class="logo">WaTch LaNe</h2>
+    </div>
+    <div class="welcome">
+        <a href="">Welcome guest</a>
+    </div>
+
+</div>
+ 
+</body>
+</html>
